3ba7ee7c59bd92fbee100c39fcf4796dc61b022a
muhammed ramshin <muhammedramshin@gmail.com>
2020-07-03 19:45:17 +0530

    completed angular routing basics and implimented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,20 @@ import { from } from 'rxjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -31,8 +38,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
-
+    MatButtonModule,
+    AppRoutingModule
     
   ],
   providers: [DishService],
