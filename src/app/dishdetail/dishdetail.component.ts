76d16f1e43e02b1ff237211608a592e7fd6f4232
muhammed ramshin <muhammedramshin@gmail.com>
2020-07-26 10:58:24 +0530

    completed animation part 2 by adding 3 animation

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,24 +6,21 @@ import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { FormGroup, FormControl } from '@angular/forms';
 import { FormBuilder,Validators } from '@angular/forms';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility, flyInOut,expand } from '../animations/app.animation';
+
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
-  ,
+  ,host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+   visibility(),
+   flyInOut(),
+   expand()
   ]
   
 })
